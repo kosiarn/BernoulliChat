2810e58bafd9ae271d2c43a40702ee2c28251b0f
Damian Stechnij <117997337+DamSte09@users.noreply.github.com>
2023-12-12 18:58:53 +0100

    testing OCR

--- OCR_test.py
+++ OCR_test.py
@@ -1,3 +1,23 @@
 import pytesseract
 import cv2
+import numpy as np
+from PIL import Image
+import requests
+
+
+def get_string(img_path):
+    img = cv2.imread(img_path)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    kernel = np.ones((1, 1), np.uint8)
+    img = cv2.dilate(img, kernel, iterations=1)
+    img = cv2.erode(img, kernel, iterations=1)
+    cv2.imwrite("removed_noise.png", img)
+    cv2.imwrite(img_path, img)   
+
+    result = pytesseract.image_to_string(Image.open(img_path), lang="pol")
+    return result
+
+img_path = 'tekst.jpg'
+pytesseract.pytesseract.tesseract_cmd = r'C:/Users/pc/AppData/Local/Programs/Tesseract-OCR/tesseract.exe'
+print(get_string(img_path))
 
